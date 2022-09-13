c025294a3b4c7e05fe460843a84e5a27246970d8
Cypis <arturnowosad@gmail.com>
2022-09-13 20:21:13 +0200

    v2-6

--- src/components/contact/contact.componente.js
+++ src/components/contact/contact.componente.js
@@ -64,18 +64,13 @@ const ContactUs = () => {
                     <div className='contact-box-form'>
                         <div className="contact-enquiry">{t('form.title')}</div>
 
-                        <form name="contact" method="POST" data-netlify="true">
+                        <form name="contact" method="post">
+                            <input type="hidden" name="form-name" value="contact" />
                             <p>
                                 <label>Your Name: <input type="text" name="name" /></label>
                             </p>
                             <p>
                                 <label>Your Email: <input type="email" name="email" /></label>
-                            </p>
-                            <p>
-                                <label>Your Role: <select name="role[]" multiple>
-                                    <option value="leader">Leader</option>
-                                    <option value="follower">Follower</option>
-                                </select></label>
                             </p>
                             <p>
                                 <label>Message: <textarea name="message"></textarea></label>
